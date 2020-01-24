8810ab51a55bcfe8f9c2bee84b21238f74f8c0b0
DAM_1 <DAM_1@PB09>
2020-01-24 19:42:15 +0100

    Mejora 1

--- src/main/java/Arrays.java
+++ src/main/java/Arrays.java
@@ -4,6 +4,17 @@ public class Arrays {
 
 	public static void main(String[] args) {
 		int array[]={1,5,4,9,3};
+		int numeroMaximo=maximo(array);
+		System.out.println(numeroMaximo);
+	}
+	public static int maximo(int[]maximo) {
+		int numeroMaximo=0;
+		for(int i=0;i<maximo.length;i++) {
+			if(maximo[i]>numeroMaximo) {
+				numeroMaximo=maximo[i];
+			}
+		}
+		return numeroMaximo;
 	}
 
 }
