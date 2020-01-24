99ccaf345f7cb031a2de98f3c3cf09d860b6270b
DAM_1 <DAM_1@PB07>
2020-01-24 20:48:31 +0100

    cambio

--- src/main/java/Numero.java
+++ src/main/java/Numero.java
@@ -1,7 +1,21 @@
 package main.java;
 
 public class Numero {
+	
+	public static String comprobarPrimo(int numero) {
+		String primo="";
+		for(int i=2;i<=numero-1;i++) {
+			if(numero%i==0) {
+				primo = "si";
+			}else {
+				primo = "no";
+			}
+		}
+		return primo;
+	}
+	
 	public static void main(String[] args) {
 		int numero=497;
+		System.out.println(comprobarPrimo(numero));
 	}
 }
