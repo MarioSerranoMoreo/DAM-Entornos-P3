a804a3d9a4e175695833c6abbe18d589a232d8d3
DAM_1 <DAM_1@PB07>
2020-01-24 20:23:41 +0100

    # WARNING: head commit changed in the meantime
    cambio

--- src/main/java/Arrays.java
+++ src/main/java/Arrays.java
@@ -1,9 +1,22 @@
 package main.java;
 
 public class Arrays {
-
+	
+	public static int mediana(int [] array) {
+		int mediana=0;
+		java.util.Arrays.sort(array);
+		if(array.length%2!=0) {
+			mediana=array[array.length/2];
+		}else {
+			mediana=(array[array.length/2]+array[array.length/2-1])/2;
+		}
+		
+		return mediana;
+	}
+	
 	public static void main(String[] args) {
 		int array[]={1,5,4,9,3};
+		System.out.println(mediana(array));
 	}
 
 }
