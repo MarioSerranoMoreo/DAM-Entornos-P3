9edd35651141dcdb581df5820047ce56b46e3bda
Familia Ojeda <Familia Ojeda@DESKTOP-5UIP68O>
2020-01-31 19:49:07 +0100

    Mejora 6

--- src/main/java/Cadena.java
+++ src/main/java/Cadena.java
@@ -6,7 +6,12 @@ public class Cadena {
 		String palabra="hola";
 		System.out.println(palabra.toLowerCase());
 		System.out.println(palabra.toUpperCase());
+		longitud(palabra);
 
 	}
-
+	public static void longitud(String palabra) {
+		 	int length = palabra.length();       
+	        System.out.println("Longitud de una cadena es:" + length); 
+		
+	}
 }
