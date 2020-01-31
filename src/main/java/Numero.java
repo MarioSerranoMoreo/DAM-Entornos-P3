06623f58ee96c677c38edd731356594112c83402
DAM_1 <DAM_1@PB07>
2020-01-31 18:53:03 +0100

    cambio

--- src/main/java/Numero.java
+++ src/main/java/Numero.java
@@ -1,7 +1,18 @@
 package main.java;
 
 public class Numero {
+	
+	public static int factorial(int numero) {
+		int factorial=1;
+		for(int i=1;i<=numero;i++) {
+			factorial=factorial*i;
+		}
+		return factorial;
+	}
+	
 	public static void main(String[] args) {
-		int numero=497;
+		int numero=19;
+		//497
+		System.out.println("El factorial es "+factorial(numero));
 	}
 }
