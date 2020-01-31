1e1955c4d7b33f87278adc85d3cec1c7f852293c
Familia Ojeda <Familia Ojeda@DESKTOP-5UIP68O>
2020-01-31 19:58:36 +0100

    Mejora_6

--- src/main/java/Cadena.java
+++ src/main/java/Cadena.java
@@ -4,8 +4,6 @@ public class Cadena {
 
 	public static void main(String[] args) {
 		String palabra="hola";
-		System.out.println(palabra.toLowerCase());
-		System.out.println(palabra.toUpperCase());
 		longitud(palabra);
 
 	}
